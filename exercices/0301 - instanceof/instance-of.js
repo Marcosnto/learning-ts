5512bcdfa24994f682ba89bee0ebf13259edb3e1
Marcos Neto <marcosnto@MacBook-Air-de-Marcos.local>
2023-03-29 12:28:29 -0300

    0301 - exercise instanceof

---
+++ exercices/0301 - instanceof/instance-of.js
@@ -0,0 +1,7 @@
+"use strict";
+// https://www.origamid.com/slide/typescript-para-iniciantes/#/0301-instanceof/2
+// instanceof
+// Existem funções que retornam diferentes tipos de objetos.
+// Com a palavra-chave instanceof podemos verificar se um objeto é uma instância
+// (foi construído ou herda) de uma função construtora (class).
+// O instanceof verifica se a função construtora herda de outra (extends).
